3f9f28aefef099ffc6d4f0df3a37a9eef61d87fa
vijetha3017 <vijetha3017@gmail.com>
2024-01-17 20:35:51 +0530

    first java project-jan2024

---
+++ src/main/java/com/careerit/jan2024/SumOfTwoNumbers1.java
@@ -0,0 +1,9 @@
+package com.careerit.jan2024;
+
+public class SumOfTwoNumbers1 {
+    public static void main(String[] args) {
+        int a=5,b=6;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
