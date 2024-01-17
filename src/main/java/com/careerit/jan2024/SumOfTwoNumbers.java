eb49b57abef34307abed7042d98682eb180e805d
vijetha3017 <vijetha3017@gmail.com>
2024-01-17 20:33:28 +0530

    first java project-jan2024

--- src/main/java/com/careerit/jan2024/SumOfTwoNumbers.java
+++ src/main/java/com/careerit/jan2024/SumOfTwoNumbers.java
@@ -2,8 +2,8 @@ package com.careerit.jan2024;
 
 public class SumOfTwoNumbers {
     public static void main(String[] args) {
-        int num1=10,num2=20;
-        int sum=num1+num2;
-        System.out.println(num1+"+"+num2+"= "+sum);
+        int a=5,b=6;
+        int sum=a+b;
+        System.out.println(sum);
     }
 }
