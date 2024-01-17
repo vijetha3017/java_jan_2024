909594a0eb91b3f9a336f3f23c1fd602368a6d9b
vijetha3017 <vijetha3017@gmail.com>
2024-01-17 20:13:08 +0530

    added day1 examples

---
+++ src/main/java/com/careerit/jan2024/SumOfTwoNumbers.java
@@ -0,0 +1,9 @@
+package com.careerit.jan2024;
+
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        int num1=10,num2=20;
+        int sum=num1+num2;
+        System.out.println(num1+"+"+num2+"= "+sum);
+    }
+}
