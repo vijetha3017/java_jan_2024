abad4c93fa4b8e20070b7b8db84ef5c2e09ad74d
vijetha3017 <vijetha3017@gmail.com>
2024-01-18 20:52:03 +0530

    altered first java project-jan2024

--- src/main/java/com/careerit/jan2024/Sum1.java
+++ src/main/java/com/careerit/jan2024/Sum1.java
@@ -3,5 +3,7 @@ package com.careerit.jan2024;
 public class Sum1 {
     public static void main(String[] args) {
         System.out.println(5+6);
+        System.out.println("hello"+(5+60));
+
     }
 }
