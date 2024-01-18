679ee36371414312b8b3c0ed47bbe1bbe4fa5b4a
vijetha3017 <vijetha3017@gmail.com>
2024-01-18 20:55:12 +0530

    altered 1 first java project-jan2024

--- src/main/java/com/careerit/jan2024/First.java
+++ src/main/java/com/careerit/jan2024/First.java
@@ -3,5 +3,6 @@ package com.careerit.jan2024;
 public class First {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello"+(50+60));
     }
 }
