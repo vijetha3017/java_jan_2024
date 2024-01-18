efecb4f57b7bbc8336fe71cc6ef7e978d715ab80
vijetha3017 <vijetha3017@gmail.com>
2024-01-18 21:00:17 +0530

    altered 1 first java project-jan2024

--- src/main/java/com/careerit/jan2024/Sum.java
+++ src/main/java/com/careerit/jan2024/Sum.java
@@ -4,6 +4,6 @@ public class Sum {
     public static void main(String[] args) {
         int a = 5, b = 6;
         int c = a + b;
-        System.out.println("a+b=" + c);
+        System.out.println( c);
     }
 }
