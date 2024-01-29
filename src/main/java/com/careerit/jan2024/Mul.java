1ce94732126beed107833476658b06c9d529e7c8
vijetha3017 <154362068+vijetha3017@users.noreply.github.com>
2024-01-29 17:49:33 +0530

    Update Mul.java
    added a statement in " "

--- src/main/java/com/careerit/jan2024/Mul.java
+++ src/main/java/com/careerit/jan2024/Mul.java
@@ -4,7 +4,7 @@ public class Mul {
     public static void main(String[] args) {
         int n=11;
         for(int i=1;i<=10;i++){
-            System.out.println(n*i);
+            System.out.println("multiplication table of 11"+n*i);
         }
     }
 }
