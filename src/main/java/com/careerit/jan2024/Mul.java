947a01ff9b85e345826393de1116b198d4dab679
vijetha3017 <vijetha3017@gmail.com>
2024-01-29 17:42:23 +0530

    added extra log msg

---
+++ src/main/java/com/careerit/jan2024/Mul.java
@@ -0,0 +1,10 @@
+package com.careerit.jan2024;
+
+public class Mul {
+    public static void main(String[] args) {
+        int n=11;
+        for(int i=1;i<=10;i++){
+            System.out.println(n*i);
+        }
+    }
+}
