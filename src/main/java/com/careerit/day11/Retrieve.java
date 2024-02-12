ee7169631893175e2d79bbbaa271474f8731db33
vijetha3017 <vijetha3017@gmail.com>
2024-02-12 19:46:43 +0530

    demo on csv--day 11

---
+++ src/main/java/com/careerit/day11/Retrieve.java
@@ -0,0 +1,9 @@
+package com.careerit.day11;
+
+public class Retrieve {
+    public static void main(String[] args) {
+        Retrievecsv ob1= new Retrievecsv("Indiana","Monroe",8607);
+        Retrievecsv ob2= new Retrievecsv();
+        ob1.showDetails();
+    }
+}
